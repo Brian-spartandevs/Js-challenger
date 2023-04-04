1a6b9a0647bf61dc7e55c7fa4c997b64f698c969
Brian Arrieta <brian@spartandevs.com>
2023-04-04 18:02:52 -0400

    modificacion de Test 12

--- Javascript Fundamentals/Test 12/script.js
+++ Javascript Fundamentals/Test 12/script.js
@@ -3,6 +3,10 @@
         Write a function that takes 6 values (a,b,c,d,e,f) as arguments. Sum a and b. Then substract by c. Then multiply by d and divide by e. Finally raise to the power of f and return the result. Tipp: mind the order.
 */
 
+/* const mathOperator = (a, b, c, d, e, f) => {
+  return (((a + b - c) * d) / e) ** f;
+} */
+
 const mathOperator = (a, b, c, d, e, f) => {
   let result = (((a + b) - c) * d) / e;
   result = Math.pow(result, f);
